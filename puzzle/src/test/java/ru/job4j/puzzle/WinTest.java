889184daf0b87f034d30266c11836fee868d56b8
Николай <kolia047@mail.ru>
2022-05-16 15:02:25 +0600

    2. Сокобан. Тестовое задание. [#75283 #301498]

--- puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
+++ puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
@@ -1,10 +1,8 @@
 package ru.job4j.puzzle;
 
-import org.junit.Ignore;
 import org.junit.Test;
-
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
 
 public class WinTest {
     @Test
